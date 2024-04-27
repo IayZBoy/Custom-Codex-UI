79a7f29ae44429dceee2f8d98db62053587415bc
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-04-27 17:44:07 +0200

    Create CustomUI.lua

---
+++ CustomUI.lua
@@ -0,0 +1,15 @@
+_G.CodexUiCustomSettings = {
+CodexSpooferImage = 17305003173, 
+CodexSpooferText = "Z-Exec", 
+CodexSpooferPoweredBy = "Powered By: \nZBoy & SPDM Team", 
+FloatingIconColor=Color3.fromRGB(25,25,25), 
+MainUiBackground = Color3.fromRGB(25,25,25), 
+Icons=Color3.fromRGB(255,255,255), 
+IconsOff = Color3.fromRGB(255,255,255), 
+Fade = false
+}
+
+_G.CodexSpooferImage = nil
+_G.CodexSpooferText = nil
+_G.CodexSpooferPoweredBy = nil 
+loadstring(game:HttpGet("https://github.com/MuhXd/Roblox-mobile-script/blob/main/Codex/Synapse%20X%20Real%20Not%20Clickbait.lua?raw=true" ))();
